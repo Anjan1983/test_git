41702652b0dc4c21fe2458755c8b00c1102dcaf0
pattanaik, Anjankumar <anjankumar.pattanaik@novartis.com>
2015-09-24 12:03:19 +0530

    My first change

---
+++ DemoWebApp/DemoWebApp/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("DemoWebApp")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Novartis")> 
+<Assembly: AssemblyProduct("DemoWebApp")> 
+<Assembly: AssemblyCopyright("Copyright © Novartis 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("695a2cc3-8fd5-4f02-bf71-6b67f7319801")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
